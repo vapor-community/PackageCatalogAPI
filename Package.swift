25ad1d71a0b09b94327d7795e435f343d16f3189
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-23 16:52:00 -0600

    'created PackageCatalog from template https://github.com/vapor/api-template'

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "PackageCatalog",
+    dependencies: [
+        // 💧 A server-side Swift web framework. 
+        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
+
