4124ea771a1285d83053074e14c58126de112997
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-12-25 11:14:08 -0600

    Configured Fluent and MySQL with Application

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,6 @@
 import Vapor
+import MySQL
+import FluentMySQL
 
 /// Called before your application initializes.
 ///
@@ -8,6 +10,27 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
+    try services.provider(FluentProvider())
+    services.instance(FluentMySQLConfig())
+    
+    var dbConfig = DatabaseConfig()
+    let database = MySQLDatabase(hostname: "localhost", user: "root", password: nil, database: "package_catalog")
+    dbConfig.add(database: database, as: .mysql)
+    services.instance(dbConfig)
+    
+    var migirateConfig = MigrationConfig()
+    // Add models here:
+    // migrationConfig.add(model: MyModel.self, database: .mysql)
+    services.instance(migirateConfig)
+    
+    
     // configure your application here
+    let mysql = MySQLProvider(hostname: "127.0.0.1", user: "root", password: nil, database: "package_catalog")
+    try mysql.register(&services)
+}
 
+extension DatabaseIdentifier {
+    static var mysql: DatabaseIdentifier<MySQLDatabase> {
+        return .init("mysql")
+    }
 }
