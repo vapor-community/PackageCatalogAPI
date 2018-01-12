4f6fd567b4f9df113c816795f00f96d428e14c8c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-12 16:52:13 -0600

    Created POST /packages PackageController route

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -6,7 +6,14 @@ final class PackageController: RouteCollection {
         return Package.query(on: request).all()
     }
     
+    func create(_ request: Request)throws -> Future<Package> {
+        let package = try JSONDecoder().decode(Package.self, from: request.body)
+        _ = package.save(on: request)
+        return Future(package)
+    }
+    
     func boot(router: Router) throws {
         router.get("packages", use: index)
+        router.post("packages", use: create)
     }
 }
