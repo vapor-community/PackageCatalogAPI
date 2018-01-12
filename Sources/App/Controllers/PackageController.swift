cb423d4d4ca4b78a1e8019b4ddde605384da86b2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-12 16:47:36 -0600

    Created GET /packages PackageController route

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -1,7 +1,12 @@
 import Vapor
+import Foundation
 
 final class PackageController: RouteCollection {
+    func index(_ request: Request)throws -> Future<[Package]> {
+        return Package.query(on: request).all()
+    }
+    
     func boot(router: Router) throws {
-        
+        router.get("packages", use: index)
     }
 }
