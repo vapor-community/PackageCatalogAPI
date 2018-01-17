753c4793c196169f1b34eb90be4d8b08d4e0a927
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-17 16:58:07 -0600

    Created GET /packages/:int PackageController route

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -11,8 +11,20 @@ final class PackageController: RouteCollection {
         return package.save(on: request).transform(to: package)
     }
     
+    func getById(_ request: Request)throws -> Future<Package> {
+        let id: Int = try request.parameter()
+        let package = Package.find(id, on: request).map(to: Package.self, { (package)throws -> Package in
+            guard let package = package else {
+                throw Abort(.badRequest)
+            }
+            return package
+        })
+        return package
+    }
+    
     func boot(router: Router) throws {
         router.get("packages", use: index)
         router.post("packages", use: create)
+        router.get("packages", Int.parameter, use: getById)
     }
 }
