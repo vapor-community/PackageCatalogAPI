11ec0518be2bd8dc0fdc3ef5b22c84ee7173e276
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-01-19 17:12:59 -0600

    Removed redundant Request.DatabaseConnectable conformance

--- Sources/App/Configuration/Database.swift
+++ Sources/App/Configuration/Database.swift
@@ -1,8 +1,6 @@
 import FluentMySQL
 import Vapor
 
-extension Request: DatabaseConnectable {}
-
 extension DatabaseIdentifier {
     static var mysql: DatabaseIdentifier<MySQLDatabase> {
         return .init("mysql")
