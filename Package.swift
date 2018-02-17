eab9840dbfa8edf665b8f85bd6a5caa93e534235
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-17 12:07:41 -0600

    Updated packages to beta tags

--- Package.swift
+++ Package.swift
@@ -5,12 +5,11 @@ let package = Package(
     name: "PackageCatalogAPI",
     dependencies: [
         // 💧 A server-side Swift web framework. 
-        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
-        .package(url: "https://github.com/vapor/fluent.git", .branch("beta")),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", .branch("beta"))
+        .package(url: "https://github.com/vapor/vapor.git", "3.0.0-beta.3"..<"3.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", "1.0.0-beta.2"..<"1.0.0-beta.3")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Fluent", "FluentPostgreSQL"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
