5a34c36f8b5d1c337a99fbfe46f06974fd7140cd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-20 17:17:46 -0600

    Updated PackageController route handlers to use .public method on package models

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -5,18 +5,12 @@ import Foundation
 
 final class PackageController: RouteCollection {
     func index(_ request: Request)throws -> Future<[PackageData]> {
-        return Package.query(on: request).all().flatMap(to: [PackageData].self, { (packages) in
-            return try packages.public(with: request)
-        })
+        return Package.query(on: request).all().public(with: request)
     }
     
     func create(_ request: Request)throws -> Future<PackageData> {
         let package = try JSONDecoder().decode(Package.self, from: request.http.body)
-        return package.flatMap(to: Package.self, { (package) in
-            package.save(on: request).transform(to: package)
-        }).flatMap(to: PackageData.self, { (package) in
-            return try package.public(with: request)
-        })
+        return package.save(on: request).public(with: request)
     }
     
     func getByName(_ request: Request)throws -> Future<PackageData> {
@@ -24,26 +18,15 @@ final class PackageController: RouteCollection {
         let name = try request.parameter(String.self)
         let package = Package.query(on: request).filter(\Package.name == name).filter(\Package.owner == owner)
         
-        return package.first().flatMap(to: PackageData.self, { (pack) in
-            guard let pack = pack else {
-                throw Abort(.notFound)
-            }
-            return try pack.public(with: request)
-        })
+        return package.first().unwrap(or: Abort(.notFound)).public(with: request)
     }
     
     func delete(_ request: Request)throws -> Future<HTTPStatus> {
         let owner = try request.parameter(String.self)
         let name = try request.parameter(String.self)
         let package = Package.query(on: request).filter(\Package.name == name).filter(\Package.owner == owner)
-        return package.first().flatMap(to: Void.self, { (pack) in
-            guard let pack = pack else {
-                throw Abort(.notFound)
-            }
-            return pack.delete(on: request).transform(to: ())
-        }).map(to: HTTPStatus.self, { _ in
-            return .ok
-        })
+        
+        return package.first().unwrap(or: Abort(.notFound)).delete(on: request).transform(to: .ok)
     }
     
     func boot(router: Router) throws {
