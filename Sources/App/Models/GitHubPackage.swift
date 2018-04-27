43f626c9b3aca8d91ced525d1e4b0084e02b656e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-27 16:25:47 -0500

    Created structures for GitHub API response JSON

---
+++ Sources/App/Models/GitHubPackage.swift
@@ -0,0 +1,38 @@
+import Vapor
+
+// MARK: /owner/name
+
+struct GitHubPackage: Content {
+    let name: String
+    let owner: GitHubOwner
+    let description: String?
+    let stars: Int
+    let watchers: Int
+    let forks: Int
+    let license: GitHubLicense
+    
+    enum CodingKeys: String, CodingKey {
+        case name, owner, description, license
+        case stars = "stargazers_count"
+        case watchers = "watchers_count"
+        case forks = "forks_count"
+    }
+}
+
+struct GitHubOwner: Content {
+    let login: String
+}
+
+struct GitHubLicense: Content {
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "spdx_id"
+    }
+}
+
+// MARK: /owner/name/{branches|tags}
+
+struct GitHubNode: Content {
+    let name: String
+}
