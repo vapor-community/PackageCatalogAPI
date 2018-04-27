d1d0105011e9342e30b89f9af6c36fe9a99cef94
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-27 15:37:17 -0500

    Created GET /packages/:package PackageController route

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -4,12 +4,24 @@ import Fluent
 import Foundation
 
 final class PackageController: RouteCollection {
+    func boot(router: Router) throws {
+        router.post(Package.self, at: "packages", use: create)
+        router.get("packages", use: index)
+        router.get("packages", Package.parameter, use: read)
+        router.get("packages", String.parameter, String.parameter, use: getByName)
+        router.delete("packages", String.parameter, String.parameter, use: delete)
+    }
+    
     func index(_ request: Request)throws -> Future<[Package]> {
         return Package.query(on: request).all()
     }
     
     func create(_ request: Request, _ package: Package)throws -> Future<Package> {
         return package.save(on: request)
+    }
+    
+    func read(_ request: Request)throws -> Future<Package> {
+        return try request.parameters.next(Package.self)
     }
     
     func getByName(_ request: Request)throws -> Future<Package> {
@@ -26,12 +38,5 @@ final class PackageController: RouteCollection {
         let package = try Package.query(on: request).filter(\Package.name == name).filter(\Package.owner == owner)
         
         return package.first().unwrap(or: Abort(.notFound)).delete(on: request).transform(to: .ok)
-    }
-    
-    func boot(router: Router) throws {
-        router.get("packages", use: index)
-        router.post(Package.self, at: "packages", use: create)
-        router.get("packages", String.parameter, String.parameter, use: getByName)
-        router.delete("packages", String.parameter, String.parameter, use: delete)
     }
 }
