c46d27507a59a81d38b52bc78ee64ce33f03c172
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-28 15:58:57 -0500

    Installed Manifest package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework. 
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/Ether-CLI/Manifest.git", from: "0.3.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL", "Manifest"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
