eaa916b147c9e52b06e11f97ec422b7178285afa
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-30 16:42:49 -0500

    Installed Vapor package

--- Package.swift
+++ Package.swift
@@ -1,19 +1,13 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.0.
 
 import PackageDescription
 
 let package = Package(
     name: "PackageCatalogAPI",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "3.0.0-rc.2.8.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "PackageCatalogAPI",
-            dependencies: []),
+        .target(name: "PackageCatalogAPI", dependencies: ["Vapor"]),
     ]
 )
