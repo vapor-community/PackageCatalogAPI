bedfb2693b067d8b04b20fd4d06ac0132059059c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-30 16:50:52 -0500

    Added Vapor api-temaplte source code

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0.
+// swift-tools-version:4.0
 
 import PackageDescription
 
@@ -8,6 +8,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor", from: "3.0.0-rc.2.8.1"),
     ],
     targets: [
-        .target(name: "PackageCatalogAPI", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
