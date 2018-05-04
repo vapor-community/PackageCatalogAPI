bb6f711be655dff3c9b357477e06dec0351d253a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-04 16:55:54 -0500

    Installed Manifest package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ let package = Package(
     name: "PackageCatalogAPI",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", from: "3.0.0-rc.2.8.1"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/Ether-CLI/Manifest.git", from: "0.4.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Authentication"]),
+        .target(name: "App", dependencies: ["Vapor", "Authentication", "Manifest"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
