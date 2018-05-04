64af1fcf643fa97a617a2d5170a9808197d81ef9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-04 09:28:11 -0500

    Created GET /packages/:string/:string PackageController route

--- Sources/App/Controllers/PackageController.swift
+++ Sources/App/Controllers/PackageController.swift
@@ -1,5 +1,31 @@
 import Vapor
 
 final class PackageController: RouteCollection {
-    func boot(router: Router) throws {}
+    func boot(router: Router) throws {
+        let packages = router.grouped("packages")
+        
+        packages.get(String.parameter, String.parameter, use: get)
+    }
+    
+    func get(_ request: Request)throws -> Future<Response> {
+        let owner = try request.parameters.next(String.self)
+        let repo = try request.parameters.next(String.self)
+        let client = try request.make(Client.self)
+        
+        let manifest = client.get("https://raw.githubusercontent.com/\(owner)/\(repo)/master/Package.swift")
+        return manifest.flatMap(to: Response.self) { response in
+            guard response.http.status.code / 100 == 2 else {
+                throw Abort(.notFound, reason: "No package found with name '\(owner)/\(repo)'")
+            }
+            return client.get("https://api.github.com/repos/\(owner)/\(repo)")
+        }.map(to: Response.self) { response in
+            guard let data = response.http.body.data else {
+                throw Abort(.notFound, reason: "No package found with name '\(owner)/\(repo)'")
+            }
+            let response = request.makeResponse()
+            response.http.body = HTTPBody(data: data)
+            response.http.headers.replaceOrAdd(name: .contentType, value: "application/json")
+            return response
+        }
+    }
 }
