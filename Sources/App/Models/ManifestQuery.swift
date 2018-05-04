f5d14ac4231296dbc6d4064dd90e4ecbe7f43c94
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-04 17:23:54 -0500

    Removed 'file' decoder container level from ManifestExtractor(from:) decoder init

--- Sources/App/Models/ManifestQuery.swift
+++ Sources/App/Models/ManifestQuery.swift
@@ -38,7 +38,6 @@ struct ManifestExtractor: Content {
         let container = try decoder.container(keyedBy: SubKeys.self)
         let data = try container.nestedContainer(keyedBy: SubKeys.self, forKey: .data)
         let repo = try data.nestedContainer(keyedBy: SubKeys.self, forKey: .repository)
-        let file = try repo.nestedContainer(keyedBy: CodingKeys.self, forKey: .file)
-        self.manifest = try file.decode(Manifest.self, forKey: .manifest)
+        self.manifest = try repo.decode(Manifest.self, forKey: .file)
     }
 }
