1e98e85527a9fedc76c0ca0768a62a1a7e86910c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-04 11:50:36 -0500

    Installed Auth package

--- Package.swift
+++ Package.swift
@@ -6,9 +6,10 @@ let package = Package(
     name: "PackageCatalogAPI",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", from: "3.0.0-rc.2.8.1"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
