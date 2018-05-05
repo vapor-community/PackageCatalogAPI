6cf262f9d3b358b15c72d67805215ca3c0c165e1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-05 10:45:52 -0500

    Added test outline

---
+++ Tests/AppTests/AppTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import App
+
+final class AppyTests: XCTestCase {
+    func testExample()throws {}
+    
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
